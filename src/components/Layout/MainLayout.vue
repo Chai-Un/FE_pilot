04cf3c8c8d2bceaa9f9028137bde0165d28265cf
Cuong Tran <quangcuong0808@gmail.com>
2023-02-28 01:35:37 +0700

    Format lint

--- src/components/Layout/MainLayout.vue
+++ src/components/Layout/MainLayout.vue
@@ -1,7 +1,9 @@
 <template>
   <MainHeader></MainHeader>
   <router-view />
+  <MainFooter></MainFooter>
 </template>
 <script lang="ts" setup>
 import MainHeader from '@/components/Common/MainHeader.vue'
+import MainFooter from '@/components/Common/MainFooter.vue'
 </script>
